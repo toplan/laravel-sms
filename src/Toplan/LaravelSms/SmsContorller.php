41a7dc6884d23849dfcab787ca534528d6d2d120
toplan <toplan710@gmail.com>
2015-06-15 11:15:08 +0800

    优化验证码短信调试功能

--- src/Toplan/LaravelSms/SmsContorller.php
+++ src/Toplan/LaravelSms/SmsContorller.php
@@ -81,8 +81,16 @@ class SmsController extends Controller {
 
     public function getInfo()
     {
-        echo ('<p>hello, welcome to laravel-sms for l5.  current time:'.time().'</p><hr><p style="color: green;">sms data in session:</p> <br>');
-        dd(SmsManager::getSmsDataFromSession());
+        $html = '<h2 align="center" style="margin-top: 20px;">Hello, welcome to laravel-sms for l5.</h2>';
+        $html .= '<p style="color: #666;"><a href="https://github.com/toplan/laravel-sms" target="_blank">laravel-sms源码</a>托管在GitHub，欢迎你的使用。如有问题和建议，欢迎提供issue。当然你也能为该项目提供开源代码，让laravel-sms支持更多服务商。</p>';
+        $html .= '<hr>';
+        $html .= '<p>你可以在调试模式(设置config/app.php中的debug为true)下查看到存储在session中的验证码短信相关数据(方便你进行调试)：</p>';
+        echo $html;
+        if (config('app.debug')) {
+            dd(SmsManager::getSmsDataFromSession());
+        } else {
+            echo '<p align="center" style="color: #ff0000;;">现在是非调试模式，无法查看验证码短信数据</p>';
+        }
     }
 
 }
