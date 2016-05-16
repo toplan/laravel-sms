a31bee2081cc051bf10fd6eb8fb8b78b115b2e7e
lan tian peng <toplan710@gmail.com>
2016-05-16 02:18:33 -0400

    Applied fixes from StyleCI

--- src/config/laravel-sms.php
+++ src/config/laravel-sms.php
@@ -32,7 +32,7 @@ return [
             'default' => 'mobile_required',
             //available static rules:
             'rules' => [
-                'mobile_required' => 'required|zh_mobile',
+                'mobile_required'     => 'required|zh_mobile',
                 'check_mobile_unique' => 'required|zh_mobile|unique:users,mobile',
                 'check_mobile_exists' => 'required|zh_mobile|exists:users',
             ],
@@ -73,8 +73,8 @@ return [
     |
     */
     'verifySmsContent' => '【your app signature】亲爱的用户，您的验证码是%s。有效期为%s分钟，请尽快验证',
-    'codeLength' => 5,
-    'codeValidTime' => 5,
+    'codeLength'       => 5,
+    'codeValidTime'    => 5,
 
     /*
     |--------------------------------------------------------------------------
@@ -85,7 +85,7 @@ return [
     | storage: 存储方式,内置可选的有'Toplan\Sms\SessionStorage'和'Toplan\Sms\CacheStorage'
     |
     */
-    'prefix' => 'laravel_sms',
+    'prefix'  => 'laravel_sms',
     'storage' => 'Toplan\Sms\SessionStorage',
 
     /*
