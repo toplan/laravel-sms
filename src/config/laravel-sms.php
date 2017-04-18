f86ac74c4d5c459f6e6492c593813300dcf9b368
urionz <urionz@urionz.com>
2017-04-18 16:43:39 +0800

    fix code format

--- src/config/laravel-sms.php
+++ src/config/laravel-sms.php
@@ -11,12 +11,12 @@ return [
     | 如果是api应用建议`middleware`为`api`
     |
     */
-    'route' => [
-        'default' => true,//开启默认路由
+    'route'  =>  [
+        'default'    => true, //开启默认路由
         'attributes' => [
-            'prefix' => 'laravel-sms',
-            'middleware' => 'web'
-        ]
+            'prefix'     => 'laravel-sms',
+            'middleware' => 'web',
+        ],
     ],
 
     /*
